35fa3feaacf1cad542e7399b8adbb457ac9f69ac
roundseven <oneroundseven@gmail.com>
2018-01-11 19:51:15 +0800

    add foundation framework

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 
+// home
 router.get('/', async (ctx, next) => {
     await ctx.render('index', {
-        title: 'Hello Koa 2!',
+        title: 'Bugs Rate',
         allTask: ctx._jiraTask
-    })
-})
+    });
+});
 
-router.get('/string', async (ctx, next) => {
-    ctx.body = 'koa2 string'
-})
-
-router.get('/json', async (ctx, next) => {
-    ctx.body = {
-        title: 'koa2 json'
-    }
-})
-
-module.exports = router
+module.exports = router;
